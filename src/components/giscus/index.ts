98fe012c944bd608d112f9bcdcdbf260292bb629
YeYunXiang <1198368393@qq.com>
2025-11-25 11:02:52 +0800

    测试

---
+++ src/components/giscus/index.ts
@@ -0,0 +1,1 @@
+export { default as GiscusComment } from './Comments.astro'
---
+++ src/site.config.ts
@@ -0,0 +1,202 @@
+import type { CardListData, Config, IntegrationUserConfig, ThemeUserConfig } from 'astro-pure/types'
+
+export const theme: ThemeUserConfig = {
+  // === Basic configuration ===
+  /** Title for your website. Will be used in metadata and as browser tab title. */
+  title: 'PoetryCloud',
+  /** Will be used in index page & copyright declaration */
+  author: 'PoetryCloud',
+  /** Description metadata for your website. Can be used in page metadata. */
+  description: 'Stay hungry, stay foolish',
+  /** The default favicon for your site which should be a path to an image in the `public/` directory. */
+  favicon: '/favicon/favicon.ico',
+  /** Specify the default language for this site. */
+  locale: {
+    lang: 'en-US',
+    attrs: 'en_US',
+    // Date locale
+    dateLocale: 'en-US',
+    dateOptions: {
+      day: 'numeric',
+      month: 'short',
+      year: 'numeric'
+    }
+  },
+  /** Set a logo image to show in the homepage. */
+  logo: {
+    src: 'src/assets/avatar.png',
+    alt: 'Avatar'
+  },
+
+  // === Global configuration ===
+  titleDelimiter: '•',
+  prerender: true,
+  npmCDN: 'https://cdn.jsdelivr.net/npm',
+
+  // Still in test
+  head: [
+    /* Telegram channel */
+    // {
+    //   tag: 'meta',
+    //   attrs: { name: 'telegram:channel', content: '@cworld0_cn' },
+    //   content: ''
+    // }
+  ],
+  customCss: [],
+
+  // 菜单声明
+  /** Configure the header of your site. */
+  header: {
+    menu: [
+      { title: 'Blog', link: '/blog' },
+      // { title: 'Projects', link: '/projects' },
+      //  { title: 'Docs', link: '/docs' },
+      //  { title: 'Tags', link: '/tags' },
+      // { title: 'Notes', link: '/notes' },
+      { title: 'Archives', link: '/archives' },
+      // { title: 'Links', link: '/links' },
+      { title: 'About', link: '/about' }
+    ]
+  },
+
+
+  /** Configure the footer of your site. */
+  footer: {
+    // Year format
+    year: `© ${new Date().getFullYear()}`,
+    // year: `© 2019 - ${new Date().getFullYear()}`,
+    links: [
+      // Registration link
+      {
+        title: 'Copyright',
+        link: 'https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh-hans',
+        style: 'text-sm' // Uno/TW CSS class
+      },
+      {
+        title: 'Travelling',
+        link: 'https://www.travellings.cn/go.html',
+        style: 'text-sm'
+      },
+      // Privacy Policy link
+      {
+        title: 'Site Policy',
+        link: '/terms/list',
+        pos: 2 // position set to 2 will be appended to copyright line
+      }
+    ],
+    /** Enable displaying a “Astro & Pure theme powered” link in your site’s footer. */
+    credits: true,
+    /** Optional details about the social media accounts for this site. */
+    //social: { github: 'https://github.com/cworld1/astro-theme-pure' }
+    social: { }
+  },
+
+  content: {
+    /** External links configuration */
+    externalLinks: {
+      content: ' ↗',
+      /** Properties for the external links element */
+      properties: {
+        style: 'user-select:none'
+      }
+    },
+    /** Blog page size for pagination (optional) */
+    blogPageSize: 8,
+    // Currently support weibo, x, bluesky
+    share: ['weibo', 'x', 'bluesky']
+  }
+}
+
+export const integ: IntegrationUserConfig = {
+  // Links management
+  // See: https://astro-pure.js.org/docs/integrations/links
+  links: {
+    // Friend logbook
+    logbook: [
+      { date: '2025-10-13', content: '使用 Astro Theme Pure 主题搭建个人博客网站。' },
+      { date: '2025-10-18', content: '修改其中样式以及配置。' },
+      { date: '2025-11-23', content: '添加评论插件，开始更新博客并部署。' }
+    ],
+    // Yourself link info
+    applyTip: [
+      { name: 'Name', val: theme.title },
+      { name: 'Desc', val: theme.description || 'Null' },
+      { name: 'Link', val: 'https://www.poetrycloud.top/' },
+      { name: 'Avatar', val: 'https://www.poetrycloud.top/favicon/apple-touch-icon.png' }
+    ]
+  },
+  // Enable page search function
+  pagefind: true,
+  // Add a random quote to the footer (default on homepage footer)
+  // See: https://astro-pure.js.org/docs/integrations/advanced#web-content-render
+  quote: {
+    // https://developer.hitokoto.cn/sentence/#%E8%AF%B7%E6%B1%82%E5%9C%B0%E5%9D%80
+    // server: 'https://v1.hitokoto.cn/?c=i',
+    // target: (data) => (data as { hitokoto: string }).hitokoto || 'Error'
+    // https://github.com/lukePeavey/quotable
+    server: 'https://api.quotable.io/quotes/random?maxLength=60',
+    target: `(data) => data[0].content || 'Error'`
+  },
+  // UnoCSS typography
+  // See: https://unocss.dev/presets/typography
+  typography: {
+    class: 'prose text-base text-muted-foreground',
+    // The style of blockquote font, normal or italic (default to italic in typography)
+    blockquoteStyle: 'italic',
+    // The style of inline code block, code or modern (default to code in typography)
+    inlineCodeBlockStyle: 'modern'
+  },
+  // A lightbox library that can add zoom effect
+  // See: https://astro-pure.js.org/docs/integrations/others#medium-zoom
+  mediumZoom: {
+    enable: true, // disable it will not load the whole library
+    selector: '.prose .zoomable',
+    options: {
+      className: 'zoomable'
+    }
+  },
+  // Comment system
+  waline: {
+    enable: false,
+    // Server service link
+    server: 'https://astro-theme-pure-waline.arthals.ink/',
+    // Refer https://waline.js.org/en/guide/features/emoji.html
+    emoji: ['bmoji', 'weibo'],
+    // Refer https://waline.js.org/en/reference/client/props.html
+    additionalConfigs: {
+      // search: false,
+      pageview: true,
+      comment: true,
+      locale: {
+        reaction0: 'Like',
+        placeholder: 'Welcome to comment. (Email to receive replies. Login is unnecessary)'
+      },
+      imageUploader: false
+    }
+  }
+}
+
+export const terms: CardListData = {
+  title: 'Terms content',
+  list: [
+    {
+      title: 'Privacy Policy',
+      link: '/terms/privacy-policy'
+    },
+    {
+      title: 'Terms and Conditions',
+      link: '/terms/terms-and-conditions'
+    },
+    {
+      title: 'Copyright',
+      link: '/terms/copyright'
+    },
+    {
+      title: 'Disclaimer',
+      link: '/terms/disclaimer'
+    }
+  ]
+}
+
+const config = { ...theme, integ } as Config
+export default config
